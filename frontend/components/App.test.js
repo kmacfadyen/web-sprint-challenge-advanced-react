c06afd22ebb1d920c65d79ed899a3411cfb955a9
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-03-06 02:20:14 -0500

    completed

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { render, screen, fireEvent } from `@testing-library/react`;
+import { render, screen, fireEvent } from '@testing-library/react';
 import AppFunctional from "./AppFunctional";
+import '@testing-library/jest-dom/extend-expect';
 
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(true)
+  expect(true).toBe(true);
 })
 
 
